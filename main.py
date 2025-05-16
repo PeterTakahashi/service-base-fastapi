2a3fbd5c58c4bc046de4f92c6709e15124ac4085
PeterTakahashi <twitter333555@gmail.com>
2025-05-16 19:08:40 +0800

    fix cros origins

--- main.py
+++ main.py
@@ -11,9 +11,13 @@ app = FastAPI(
     version="1.0.0",
 )
 
+origins = [
+    "http://localhost:5173",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
