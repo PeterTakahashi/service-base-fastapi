72d787c8a2842dd5b8bb6afdeb6bc8bf55598b00
PeterTakahashi <twitter333555@gmail.com>
2025-05-16 12:45:29 +0800

    add me update

--- app/v1/schemas/user.py
+++ app/v1/schemas/user.py
@@ -1,6 +1,6 @@
 from fastapi_users import schemas
 from pydantic import EmailStr, ConfigDict, Field
-from typing import Generic
+from typing import Generic, Optional
 from fastapi_users import models
 
 
@@ -29,9 +29,16 @@ class UserCreate(schemas.CreateUpdateDictModel):
 
 
 class UserUpdate(schemas.CreateUpdateDictModel):
-    email: EmailStr = Field(
-        ...,
+    email: Optional[EmailStr] = Field(
+        default=None,
         max_length=320,
         json_schema_extra={"example": "user@example.com"},
         description="The email of the user.",
+    )
+    password: Optional[str] = Field(
+        default=None,
+        min_length=8,
+        max_length=100,
+        json_schema_extra={"example": "password123%"},
+        description="The password of the user.",
     )
--- app/v1/services/user_service.py
+++ app/v1/services/user_service.py
@@ -1,6 +1,12 @@
+from fastapi import Depends, HTTPException, Request, status
+from fastapi_users import exceptions, models
+from fastapi_users.manager import BaseUserManager
+from fastapi_users.router.common import ErrorCode
+
 from app.v1.repositories.user_repository import UserRepository
 from app.models.user import User
 from app.v1.schemas.user import UserUpdate, UserRead
+from app.core.user_setup import current_active_user
 
 
 class UserService:
@@ -10,8 +16,28 @@ class UserService:
     async def get_me(self, user: User) -> UserRead:
         return UserRead.model_validate(user)
 
-    async def update_me(self, user: User, data: UserUpdate) -> UserRead:
-        updated_user = await self.user_repository.update_user(
-            user, data.model_dump(exclude_unset=True)
-        )
-        return UserRead.model_validate(updated_user)
+    async def update_me(
+        self,
+        request: Request,
+        user_manager: BaseUserManager[models.UP, models.ID],
+        user_update: UserUpdate,
+        user: User = Depends(current_active_user),
+    ) -> UserRead:
+        try:
+            user = await user_manager.update(
+                user_update, user, safe=True, request=request # type: ignore
+            )
+            return UserRead.model_validate(user)
+        except exceptions.InvalidPasswordException as e:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail={
+                    "code": ErrorCode.UPDATE_USER_INVALID_PASSWORD,
+                    "reason": e.reason,
+                },
+            )
+        except exceptions.UserAlreadyExists:
+            raise HTTPException(
+                status.HTTP_400_BAD_REQUEST,
+                detail=ErrorCode.UPDATE_USER_EMAIL_ALREADY_EXISTS,
+            )
