9636c6e3c45311a18f36bac5d3dfbba05436d907
PeterTakahashi <twitter333555@gmail.com>
2025-05-31 18:31:32 +0800

    add base repo

--- tests/repositories/users/base/test_exsits_repository.py
+++ tests/repositories/users/base/test_exsits_repository.py
@@ -2,6 +2,7 @@ async def test_exists_user(user_repository, user):
     exists = await user_repository.exists(email=user.email)
     assert exists is True
 
+
 async def test_exists_user_not_found(user_repository, faker):
     exists = await user_repository.exists(email=faker.email())
     assert exists is False
