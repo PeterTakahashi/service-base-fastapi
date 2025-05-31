41a9a38c0ac457d5e9ec96beae208365a18ec81e
PeterTakahashi <twitter333555@gmail.com>
2025-05-31 19:20:02 +0800

    remove     async def update_user(self, user: User, update_data: dict) -> User:         for field, value in update_data.items():             setattr(user, field, value)         await self.session.commit()         await self.session.refresh(user)         return user

--- app/v1/repositories/user_repository.py
+++ app/v1/repositories/user_repository.py
@@ -6,10 +6,3 @@ from app.v1.repositories.base_repository import BaseRepository
 class UserRepository(BaseRepository):
     def __init__(self, session: AsyncSession):
         super().__init__(session, User)
-
-    async def update_user(self, user: User, update_data: dict) -> User:
-        for field, value in update_data.items():
-            setattr(user, field, value)
-        await self.session.commit()
-        await self.session.refresh(user)
-        return user
