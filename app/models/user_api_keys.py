0fe5747ff61e4cdfd705a99c322d57c0b8892ead
PeterTakahashi <twitter333555@gmail.com>
2025-06-02 11:23:49 +0800

    add model

---
+++ app/models/user_api_keys.py
@@ -0,0 +1,23 @@
+from app.db.base import Base
+from sqlalchemy import Column, String, Integer, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+from datetime import datetime
+from sqlalchemy import Uuid
+
+
+class UserApiKey(Base):
+    __tablename__ = "user_api_keys"
+
+    id = Column(Integer, primary_key=True, index=True)
+    api_key = Column(String, unique=True, index=True, nullable=False)
+    user_id = Column(Uuid, ForeignKey("users.id"))
+    expires_at = Column(DateTime, nullable=True)
+    allowed_origin = Column(String, nullable=True)
+    allowed_ip = Column(String, nullable=True)
+    created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
+    updated_at = Column(
+        DateTime, default=datetime.utcnow, onupdate=datetime.utcnow, nullable=False
+    )
+    deleted_at = Column(DateTime, nullable=True)
+
+    user = relationship("User", back_populates="api_keys")
