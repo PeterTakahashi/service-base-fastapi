9e649f50675557c0b389c15ccc084fd9e0eae482
PeterTakahashi <twitter333555@gmail.com>
2025-06-02 12:52:59 +0800

    add soft delete

--- app/v1/repositories/user_api_key_repository.py
+++ app/v1/repositories/user_api_key_repository.py
@@ -1,6 +1,7 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.models.user_api_key import UserApiKey
 from app.v1.repositories.base_repository import BaseRepository
+from datetime import datetime
 
 
 class UserApiKeyRepository(BaseRepository):
@@ -8,3 +9,11 @@ class UserApiKeyRepository(BaseRepository):
 
     def __init__(self, session: AsyncSession):
         super().__init__(session, UserApiKey)
+
+    def soft_delete(self, id: int):
+        """
+        Soft delete a UserApiKey by its ID.
+        Args:
+            id (int): The ID of the UserApiKey to soft delete.
+        """
+        return self.update(id=id, deleted_at=datetime.utcnow())
