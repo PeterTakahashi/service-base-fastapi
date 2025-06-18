f69c82a64f63f1423d3bae212b18c53b440a85ee
PeterTakahashi <twitter333555@gmail.com>
2025-06-18 16:03:23 +0800

    type

--- app/v1/exception_handlers/validation_exception_handler.py
+++ app/v1/exception_handlers/validation_exception_handler.py
@@ -6,22 +6,22 @@ from fastapi.responses import JSONResponse
 async def validation_exception_handler(request: Request, exc: RequestValidationError):
     errors = []
 
-    for e in exc.errors():
+    for error in exc.errors():
         parameter = ""
-        if e["loc"][0] == "body":
-            parameter = "#/" + "/".join(str(loc) for loc in e["loc"][1:])
-        elif e["loc"][0] == "query":
-            parameter = "#/" + "/".join(str(loc) for loc in e["loc"][1:])
-        elif e["loc"][0] == "path":
-            parameter = "#/" + "/".join(str(loc) for loc in e["loc"][1:])
+        if error["loc"][0] == "body":
+            parameter = "#/" + "/".join(str(loc) for loc in error["loc"][1:])
+        elif error["loc"][0] == "query":
+            parameter = "#/" + "/".join(str(loc) for loc in error["loc"][1:])
+        elif error["loc"][0] == "path":
+            parameter = "#/" + "/".join(str(loc) for loc in error["loc"][1:])
         else:
-            parameter = "#/" + "/".join(str(loc) for loc in e["loc"])
+            parameter = "#/" + "/".join(str(loc) for loc in error["loc"])
 
         errors.append(
             {
                 "code": "validation_error",
                 "title": "Validation Error",
-                "detail": e["msg"],
+                "detail": error["msg"],
                 "source": {"parameter": parameter},
             }
         )
