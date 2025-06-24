ce23526c224122e0bf0dae2707a52e12cac3164a
PeterTakahashi <twitter333555@gmail.com>
2025-06-25 01:39:58 +0800

    add OAuthAccount

--- app/v1/repositories/oauth_account_repository.py
+++ app/v1/repositories/oauth_account_repository.py
@@ -1,8 +1,8 @@
 from sqlalchemy.ext.asyncio import AsyncSession
-from app.models.oauth_account import OauthAccount
+from app.models.oauth_account import OAuthAccount
 from app.v1.repositories.base_repository import BaseRepository
 
 
 class OauthAccountRepository(BaseRepository):
     def __init__(self, session: AsyncSession):
-        super().__init__(session, OauthAccount)
+        super().__init__(session, OAuthAccount)
