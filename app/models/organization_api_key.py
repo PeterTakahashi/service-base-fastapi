4925318bbeb7c034e62dfdd4901fd1408f7ad443
PeterTakahashi <twitter333555@gmail.com>
2025-06-26 14:44:28 +0800

    add api key mixin

--- app/models/organization_api_key.py
+++ app/models/organization_api_key.py
@@ -5,29 +5,20 @@ from datetime import datetime
 from sqlalchemy import Uuid
 from typing import TYPE_CHECKING
 from app.models.mixin.timestamp import TimestampMixin
+from app.models.mixin.api_key import ApiKeyMixin
 
 if TYPE_CHECKING:
     from app.models.organization import Organization
     from app.models.user import User
 
 
-class OrganizationApiKey(TimestampMixin, Base):
+class OrganizationApiKey(TimestampMixin, ApiKeyMixin, Base):
     __tablename__ = "organization_api_keys"
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    name: Mapped[str] = mapped_column(String, nullable=False)
-    api_key: Mapped[str] = mapped_column(
-        String, unique=True, index=True, nullable=False
-    )
     organization_id: Mapped[Uuid] = mapped_column(
         ForeignKey("organizations.id"), nullable=False
     )
-    expires_at: Mapped[datetime | None] = mapped_column(
-        DateTime(timezone=True), nullable=True
-    )
-    allowed_origin: Mapped[str | None] = mapped_column(String, nullable=True)
-    allowed_ip: Mapped[str | None] = mapped_column(String, nullable=True)
-
     created_by_user_id: Mapped[Uuid] = mapped_column(
         ForeignKey("users.id"), nullable=False
     )
--- app/models/user_api_key.py
+++ app/models/user_api_key.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import relationship, Mapped, mapped_column
 from datetime import datetime
 from sqlalchemy import Uuid
 from app.models.mixin.timestamp import TimestampMixin
+from app.models.mixin.api_key import ApiKeyMixin
 
 from typing import TYPE_CHECKING
 
@@ -11,20 +12,11 @@ if TYPE_CHECKING:
     from app.models.user import User
 
 
-class UserApiKey(TimestampMixin, Base):
+class UserApiKey(TimestampMixin, ApiKeyMixin, Base):
     __tablename__ = "user_api_keys"
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    name: Mapped[str] = mapped_column(String, nullable=False)
-    api_key: Mapped[str] = mapped_column(
-        String, unique=True, index=True, nullable=False
-    )
     user_id: Mapped[Uuid] = mapped_column(ForeignKey("users.id"), nullable=False)
-    expires_at: Mapped[datetime | None] = mapped_column(
-        DateTime(timezone=True), nullable=True
-    )
-    allowed_origin: Mapped[str | None] = mapped_column(String, nullable=True)
-    allowed_ip: Mapped[str | None] = mapped_column(String, nullable=True)
     deleted_at: Mapped[datetime | None] = mapped_column(
         DateTime(timezone=True), nullable=True
     )
